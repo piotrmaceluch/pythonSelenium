8b700312b339b6b37d2fc35fe700467288ee8622
piotrmaceluch <piotrmaceluch@gmail.com>
2022-12-20 00:28:51 +0100

    environment working, ready for tests

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import time
 
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
 def waitSeconds(howLong):
     time.sleep(howLong)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-driver = webdriver.Chrome()
 driver.get("https://www.selenium.dev/documentation/webdriver/getting_started/first_script/")
 waitSeconds(3)
+
+"""
+
+"""
