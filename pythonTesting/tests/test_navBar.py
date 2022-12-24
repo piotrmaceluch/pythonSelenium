249cc629a1cc4f0d14e6e55bee1e2fa6a7f31828
piotrmaceluch <piotrmaceluch@gmail.com>
2022-12-25 00:23:17 +0100

    +navigation bar tests

--- pythonTesting/tests/test_navBar.py
+++ pythonTesting/tests/test_navBar.py
@@ -13,25 +13,33 @@ import helpers
 #
 # class Portfolio:
 @pytest.mark.positive
-def test_portfolioText():
+def test_portfolioIsDisplayed():
     driver: WebDriver = webdriver.Chrome(
         service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
     driver.get("https://selectors.webdriveruniversity.com/#")
     helpers.waitSeconds(3)
 
-    portfolioHeaderSelector = "//section[@id='portfolio']//h2[.='Portfolio']"
-    portfolioHeaderElement = driver.find_element(By.XPATH, portfolioHeaderSelector)
-    assert portfolioHeaderElement.is_displayed()
+    portfolioSelector = "PORTFOLIO"
+    portfolioElement = driver.find_element(By.LINK_TEXT, portfolioSelector)
+    assert portfolioElement.is_displayed()
 
 @pytest.mark.positive
-def test_starIsDisplayed():
+def test_playgroundIsDisplayed():
     driver: WebDriver = webdriver.Chrome(
         service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
     driver.get("https://selectors.webdriveruniversity.com/#")
     helpers.waitSeconds(3)
 
-    starSelector = "#portfolio .divider-custom"
-    starElement = driver.find_element(By.CSS_SELECTOR, starSelector)
-    assert starElement.is_displayed()
+    playgroundSelector = "PLAYGROUND"
+    playgroundElement = driver.find_element(By.LINK_TEXT, playgroundSelector)
+    assert playgroundElement.is_displayed()
+@pytest.mark.positive
+def test_contactIsDisplayed():
+    driver: WebDriver = webdriver.Chrome(
+        service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    driver.get("https://selectors.webdriveruniversity.com/#")
+    helpers.waitSeconds(3)
 
-
+    contactSelector = "CONTACT"
+    contactElement = driver.find_element(By.LINK_TEXT, contactSelector)
+    assert contactElement.is_displayed()
--- pythonTesting/tests/test_selectorPlayground.py
+++ pythonTesting/tests/test_selectorPlayground.py
@@ -11,34 +11,45 @@ import helpers
 
 
 @pytest.mark.positive
-def test_monitorImage():
+def test_monitorImageIsDisplayed():
     driver: WebDriver = webdriver.Chrome(
         service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
     driver.get("https://selectors.webdriveruniversity.com/#")
     helpers.waitSeconds(3)
 
-    playground = "[alt='code-icon']"
-    playgroundHeader = driver.find_element(By.CSS_SELECTOR, playground)
-    assert playgroundHeader.is_displayed()
+    imageSelector = "[alt='code-icon']"
+    playgroundHeaderElement = driver.find_element(By.CSS_SELECTOR, imageSelector)
+    assert playgroundHeaderElement.is_displayed()
 
 @pytest.mark.positive
-def test_selectorPlaygroundText():
+def test_selectorPlaygroundTextIsDisplayed():
     driver: WebDriver = webdriver.Chrome(
         service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
     driver.get("https://selectors.webdriveruniversity.com/#")
     helpers.waitSeconds(3)
 
-    text = "/html//body[@id='page-top']//h1[.='Selector Playground']"
-    playgroundHeader = driver.find_element(By.XPATH, text)
-    assert playgroundHeader.is_displayed()
+    textSelector = "/html//body[@id='page-top']//h1[.='Selector Playground']"
+    playgroundTextElement = driver.find_element(By.XPATH, textSelector)
+    assert playgroundTextElement.is_displayed()
 
 @pytest.mark.positive
-def test_seleniumCypressWebdriverText():
+def test_starIsDisplayed():
     driver: WebDriver = webdriver.Chrome(
         service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
     driver.get("https://selectors.webdriveruniversity.com/#")
     helpers.waitSeconds(3)
 
-    text = "/html//body[@id='page-top']//p[.='Selenium WebDriver - Cypress - WebdriverIO']"
-    playgroundHeader = driver.find_element(By.XPATH, text)
-    assert playgroundHeader.is_displayed()
+    starSelector = ".align-items-center .divider-custom"
+    starElement = driver.find_element(By.CSS_SELECTOR, starSelector)
+    assert starElement.is_displayed()
+
+@pytest.mark.positive
+def test_seleniumCypressWebdriverTextIsDisplayed():
+    driver: WebDriver = webdriver.Chrome(
+        service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    driver.get("https://selectors.webdriveruniversity.com/#")
+    helpers.waitSeconds(3)
+
+    technologiesSelector = "/html//body[@id='page-top']//p[.='Selenium WebDriver - Cypress - WebdriverIO']"
+    technologiesElement = driver.find_element(By.XPATH, technologiesSelector)
+    assert technologiesElement.is_displayed()\
